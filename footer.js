6c2ff52be153ef2c67c372b6fbd34b2bc065295b
mcaonline <shoppen.cmo1004@outlook.de>
2026-02-23 23:47:38 +0100

    upd

---
+++ footer.js
@@ -0,0 +1,11 @@
+/* SSoT for website footer copyright — edit ONLY here. */
+(function () {
+  var C = 'Keynaptic GmbH';
+  var P = 'PasteSuiteAI';
+  var Y = '2026';
+  var el = document.querySelector('.footer-text');
+  if (!el) return;
+  el.textContent = el.hasAttribute('data-trademark')
+    ? 'Copyright \u00A9 ' + Y + ' ' + C + '. All rights reserved. ' + P + '\u2122 is proprietary software and a trademark of ' + C + '.'
+    : 'Copyright \u00A9 ' + Y + ' ' + C + '. All rights reserved.';
+})();
